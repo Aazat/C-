3e619d2471cc30106bb081aeb7aa2b8b2d6676f3
Aazar <shaikhaazar11@gmail.com>
2022-07-02 16:47:30 +0530

    graph2 removed

--- graphs2/node.cpp
+++
@@ -1,8 +0,0 @@
-#include "node.h"
-// #include <iostream>
-// #include "edge.h"
-
-Node::Node(const int& value){
-    this->value= value;
-    // this->adj_edge = nullptr;
-}
--- graphs2/node.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-class Edge;
-struct Node{
-    Node(const int& value);
-    int value, position;
-    std::vector<Edge*> adj_edge;
-    // std::ostream operator<<(std::ostream& os, const Node& n);
-};
